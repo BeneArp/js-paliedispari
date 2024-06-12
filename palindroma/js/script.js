8b3252c0d56b1d926485d8e09f1dace90563040d
BeneArp <benedetta.arpino@gmail.com>
2024-06-12 14:58:46 +0200

    feat: add prompt for user world

--- palindroma/js/script.js
+++ palindroma/js/script.js
@@ -2,4 +2,10 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-console.log("ciao");
+// creo una variabile con la parola iserita dall'utente tramite prompt
+const parolaUtente = prompt("Inserisci una parola");
+console.log(parolaUtente);
+
+
+
+
